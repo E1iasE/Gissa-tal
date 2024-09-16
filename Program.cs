4da91dc2ce7833fe00866c47a264e92ae83d4f4d
E1iasE <eler07001@utb.vaxjo.se>
2024-09-16 10:12:39 +0200

    Gissningspel

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Random random = new Random();
+int randomNumber= random.Next (0,101);
+bool gissat_ratt = false; 
+int antal_forsok = -1;
+
+Console.WriteLine ("Gissa ett tal mellan 1 till 100");
+
+while (!gissat_ratt)
+{
+    antal_forsok++;
+    Console.Write("Din gissning: "); 
+    int gissning;
+    if (int.TryParse(Console.ReadLine(), out gissning) )
+    {
+        
+        if(gissning < randomNumber)
+        {
+            Console.WriteLine("För lågt, gissa igen");
+        }
+        else if (gissning > randomNumber)
+        {
+            Console.WriteLine ("För högt, gissa igen");
+        }
+        else 
+        { 
+            gissat_ratt = true;
+            antal_forsok++;
+            Console.WriteLine ($"Grattis du gissade rätt på {antal_forsok} försök.");
+        }
+
+    }
+}
+
